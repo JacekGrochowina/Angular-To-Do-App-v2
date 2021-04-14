64ecafcafb67613b140e183e6381ce80e486c2d0
Jacek <jacek99@vp.pl>
2021-04-14 18:30:47 +0200

    feat(app): add styles for mobile

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ToolbarComponent } from 'src/app/components/toolbar/toolbar.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 import { DialogAddTaskComponent } from 'src/app/components/dialog-add-task/dialog-add-task.component';
 import { DialogInfoTaskComponent } from 'src/app/components/dialog-info-task/dialog-info-task.component';
 import { DialogConfirmComponent } from 'src/app/components/dialog-confirm/dialog-confirm.component';
@@ -20,6 +21,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 const SharedComponents = [
   ToolbarComponent,
+  FooterComponent,
   DialogAddTaskComponent,
   DialogInfoTaskComponent,
   DialogConfirmComponent,
